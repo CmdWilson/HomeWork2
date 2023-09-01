6ba54f64a2df9937a210840f7ca4f66e7a1b64fc
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2023-09-01 19:44:15 +0300

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int A){
+    
+    int counter = Convert.ToString(A).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){advance = A - A % 10;
+      result = result + (A - advance);
+      A = A / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(A);
+Console.Write("Сумма цифр в числе: ");
+Console.Write(sumNumber); 
