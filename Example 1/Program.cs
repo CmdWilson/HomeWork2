aaf8e5e2055a3af84647726e86e38e48753abb27
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2023-09-01 19:42:09 +0300

    Add files via upload

---
+++ Example 1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("a = ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int B = int.Parse(Console.ReadLine());
+int sum = 1;
+int perm = A;
+int max = 0;
+if(B == 0)
+{
+    Console.Write("max = 1");
+}
+
+if(B > 0)
+{
+    
+    while(sum <= B-1)
+    {
+    max = perm * A; 
+    perm = max;
+    sum++;
+    }
+    Console.Write("max = ");
+    Console.WriteLine(perm);
+}
+else
+{
+    Console.Write("Степень является не четной ");
+}
